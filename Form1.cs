1fb22c415fc63bbab6b9264e044c5061438b84f3
mayamaya99 <elmi33@hotmail.com>
2023-01-31 21:59:16 -0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLinqHW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dataClasses1DataContext = new DataClasses1DataContext();
+            var orderShipper = from shipperCity in dataClasses1DataContext.Orders
+                               where shipperCity.ShipCity == "Seattle" || shipperCity.ShipCity == "Portland" 
+                               orderby shipperCity.ShipCity
+                               select new
+                               {
+                                   shipperCity.ShipCity,
+                                   shipperCity.OrderDate,
+                                   shipperCity.Shipper.CompanyName,
+                                   shipperName = shipperCity.ShipName
+
+                               };
+            dataGridView1.DataSource = orderShipper.ToList();
+        }
+    }
+}
